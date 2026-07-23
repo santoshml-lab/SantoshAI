a00b71113ccc1feeb9683cd5604d147cb542f0e4
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-23 20:57:46 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+export default function App() {
+
+  return (
+
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+
+    </Routes>
+
+  );
+
+}
