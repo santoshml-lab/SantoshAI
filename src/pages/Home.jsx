fd65624cd4014931615dce2849914a4d217d584d
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-23 23:23:33 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import ProjectsSection from "../components/ProjectsSection";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
+import GithubStats from "../components/GithubStats";
 
 export default function Home() {
 
@@ -16,6 +17,7 @@ export default function Home() {
       <About />
       <Skills />
       <Footer />
+      <GithubStats />
     </>
 
   );
