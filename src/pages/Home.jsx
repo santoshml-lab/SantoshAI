5f76fc40d3a5c365103112d3ae4e60cb17e2b6c9
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-23 22:01:06 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import ProjectsSection from "../components/ProjectsSection";
+import About from "../components/About";
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <ProjectsSection />
+      <About />
     </>
 
   );
