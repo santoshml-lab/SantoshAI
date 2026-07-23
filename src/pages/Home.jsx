5a1ad7ea61d5fc0b20c692dc7488ba9e60bcdeec
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-23 23:15:23 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import ProjectsSection from "../components/ProjectsSection";
 import About from "../components/About";
 import Skills from "../components/Skills";
+import Footer from "../components/Footer";
 
 export default function Home() {
 
@@ -14,6 +15,7 @@ export default function Home() {
       <ProjectsSection />
       <About />
       <Skills />
+      <Footer />
     </>
 
   );
