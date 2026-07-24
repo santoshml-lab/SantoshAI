e298ca446fcb613021b545b230a6fe839bbb946d
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-24 23:14:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,45 +2,46 @@ import "../styles/hero.css";
 import profile from "../assets/profile.jpg";
 
 export default function Hero() {
-
   return (
-
     <section className="hero">
 
-      <span className="hero-badge">
-        🚀 AI Developer Portfolio
-      </span>
+      <div className="hero-left">
 
-      <h1>
-        Build AI Products
-        <br />
-        That People Love.
-      </h1>
+        <span className="hero-badge">
+          🚀 AI Developer Portfolio
+        </span>
 
-      <p>
-        Modern AI Applications • Full Stack Development •
-        Machine Learning • Automation
-      </p>
+        <h1>
+          Build AI Products
+          <br />
+          That People Love.
+        </h1>
 
-      <div className="hero-buttons">
+        <p>
+          Modern AI Applications • Full Stack Development •
+          Machine Learning • Automation
+        </p>
 
-        <button className="primary-btn">
-          Explore Projects
-        </button>
+        <div className="hero-buttons">
+          <button className="primary-btn">
+            Explore Projects
+          </button>
 
-        <button className="secondary-btn">
-          GitHub
-        </button>
+          <button className="secondary-btn">
+            GitHub
+          </button>
+        </div>
 
       </div>
-      <img
-  src={profile}
-  alt="Santosh Yadav"
-  className="profile-image"
-/>
+
+      <div className="hero-right">
+        <img
+          src={profile}
+          alt="Santosh Yadav"
+          className="profile-image"
+        />
+      </div>
 
     </section>
-
   );
-
 }
